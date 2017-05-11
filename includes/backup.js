44f0fda3215a856e466e56b84faf28243ed959d2
Michael Rhodes <mike.rhodes@gmail.com>
2017-05-11 12:14:23 +0100

    Further split callbacks into named functions for clarity
    #46

--- includes/backup.js
+++ includes/backup.js
@@ -9,7 +9,20 @@ const spoolchanges = require('./spoolchanges.js');
 const logfilesummary = require('./logfilesummary.js');
 const logfilegetbatches = require('./logfilegetbatches.js');
 
-// process data in batches
+/**
+ * Process a set of download batches.
+ *
+ * @param {any} dbUrl - URL of database
+ * @param {any} parallelism - number of concurrent requests to make
+ * @param {any} log - log file to drive downloads from
+ * @param {any} batches - batches to download
+ * @param {any} ee - event emitter for progress. This funciton emits
+ *  received and error events.
+ * @param {any} start - time backup started, to report deltas
+ * @param {any} grandtotal - count of documents downloaded prior to this set
+ *  of batches
+ * @param {any} callback - completion callback, (err, {total: number}).
+ */
 function processBatches(dbUrl, parallelism, log, batches, ee, start, grandtotal, callback) {
   var total = grandtotal;
 
@@ -61,7 +74,7 @@ function processBatches(dbUrl, parallelism, log, batches, ee, start, grandtotal,
   q.drain = function() {
     callback(null, {total: total});
   };
-};
+}
 
 // backup function
 module.exports = function(dbUrl, blocksize, parallelism, log, resume) {
@@ -69,19 +82,23 @@ module.exports = function(dbUrl, blocksize, parallelism, log, resume) {
     blocksize = parseInt(blocksize);
   }
   const ee = new events.EventEmitter();
-  const start = new Date().getTime();
-  const maxbatches = 50;
-  var total = 0;
+  const start = new Date().getTime();  // backup start time
+  const maxbatches = 50;  // max batches to read from log file for download at a time (prevent OOM)
+  var total = 0;  // running total of documents downloaded so far
 
-  function readFromLogFile(err, data) {
+  // Reads all remaining batches in the log file and downloads them
+  var downloadRemainingBatches = function downloadRemainingBatches(err, data) {
     // no point continuing if we have no docs
     if (err) {
       return ee.emit('error', err);
     }
 
-    var finished = false;
-    async.doUntil(function(done) {
-      logfilesummary(log, function(err, summary) {
+    var noRemainingBatches = false;
+
+    // Generate a set of batches (up to maxbatches) to download from the
+    // log file and download them. Set noRemainingBatches to `true` for last batch.
+    var downloadSingleBatchSet = function downloadSingleBatchSet(done) {
+      logfilesummary(log, function processSummary(err, summary) {
         if (!summary.changesComplete) {
           ee.emit('error', new error.BackupError(
             'IncompleteChangesInLogFile',
@@ -89,43 +106,62 @@ module.exports = function(dbUrl, blocksize, parallelism, log, resume) {
            ));
         }
         if (Object.keys(summary.batches).length === 0) {
-          finished = true;
+          noRemainingBatches = true;
           return done();
         }
 
-        // decide which batch numbers to deal with
-        var batchestofetch = [];
-        var j = 0;
-        for (var i in summary.batches) {
-          batchestofetch.push(parseInt(i));
-          j++;
-          if (j >= maxbatches) break;
-        }
+        // batch IDs are the property names of summary.batches
+        var batchesToFetch = getPropertyNames(summary.batches, maxbatches);
 
-        // fetch the batch data from file
-        logfilegetbatches(log, batchestofetch, function(err, batches) {
+        // Fetch the doc IDs for the batches in the current set to
+        // download and download them.
+        var batchSetComplete = function batchSetComplete(err, data) {
+          total = data.total;
+          done();
+        };
+        var processBatchSet = function processBatchSet(err, batches) {
           // process them in parallelised queue
-          processBatches(dbUrl, parallelism, log, batches, ee, start, total, function(err, data) {
-            total = data.total;
-            done();
-          });
-        });
+          processBatches(dbUrl, parallelism, log, batches, ee, start, total, batchSetComplete);
+        };
+        logfilegetbatches(log, batchesToFetch, processBatchSet);
       });
-    }, function() {
-      // repeat until finished
-      return finished;
-    }, function() {
+    };
+
+    // Return true if all batches in log file have been downloaded
+    var isFinished = function isFinished() { return noRemainingBatches; };
+
+    var onComplete = function onComplete() {
       ee.emit('finished', {total: total});
-    });
-  }
+    };
+
+    async.doUntil(downloadSingleBatchSet, isFinished, onComplete);
+  };
 
   // If resuming, pick up from existing log file from previous run. Otherwise,
   // create new log file and process from that.
   if (resume) {
-    readFromLogFile(null, {});
+    downloadRemainingBatches(null, {});
   } else {
-    spoolchanges(dbUrl, log, blocksize, readFromLogFile);
+    spoolchanges(dbUrl, log, blocksize, downloadRemainingBatches);
   }
 
   return ee;
 };
+
+/**
+ * Returns first N properties on an object.
+ *
+ * @param {object} obj - object with properties
+ * @param {number} count - number of properties to return
+ */
+function getPropertyNames(obj, count) {
+  // decide which batch numbers to deal with
+  var batchestofetch = [];
+  var j = 0;
+  for (var i in obj) {
+    batchestofetch.push(parseInt(i));
+    j++;
+    if (j >= count) break;
+  }
+  return batchestofetch;
+}
