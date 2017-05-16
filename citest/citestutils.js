f1bc905f81ce963742490e3d43d18e99402417a5
Rich Ellis <ricellis@users.noreply.github.com>
2017-05-16 14:13:06 +0100

    Refactored tests
    Moved common test functions and hooks into `citestutils.js`.
    Added context `dbName` and `fileName` for use by tests.
    Delete `fileName` if it exists in the `afterEach` hook.
    Added scenario function to produce test name from params.
    Improved parsing to not require .json extension.
    Added end to end backup & restore via named streams in
    addition to passthrough.
    Runtime filtering of tests based on a TEST_LIMIT env var.
    Updated timeouts using new function.
    Passed whole parameter set to util functions instead of just
    useApi.
    Added function for backup and restore via file.
    Avoided extra function argument by moving opts into params.
    Added method to copy params to avoid adding properties that persist
    into later tests.

---
+++ citest/citestutils.js
@@ -0,0 +1,227 @@
+/* global beforeEach afterEach */
+'use strict';
+
+const assert = require('assert');
+const spawn = require('child_process').spawn;
+const cloudant = require('cloudant')({url: process.env.COUCH_URL});
+const app = require('../app.js');
+const dbUrl = require('../includes/cliutils.js').databaseUrl;
+const stream = require('stream');
+const uuid = require('uuid/v4');
+const fs = require('fs');
+
+beforeEach('Create test database', function(done) {
+  // Allow 10 seconds to create the DB
+  this.timeout(10 * 1000);
+  const unique = uuid();
+  this.fileName = `${unique}`;
+  this.dbName = 'couchbackup_test_' + unique;
+  cloudant.db.create(this.dbName, function(err) {
+    if (err) {
+      done(err);
+    } else {
+      done();
+    }
+  });
+});
+
+afterEach('Delete test database', function(done) {
+  // Allow 10 seconds to delete the DB
+  this.timeout(10 * 1000);
+  fs.unlink(this.fileName, function(err) {
+    if (err) {
+      if (err.code !== 'ENOENT') {
+        console.error(`${err.code} ${err.message}`);
+      }
+    }
+  });
+  cloudant.db.destroy(this.dbName, function(err) {
+    if (err) {
+      done(err);
+    } else {
+      done();
+    }
+  });
+});
+
+function scenario(test, params) {
+  return `${test} ${(params.useApi) ? 'using API' : 'using CLI'}`;
+}
+
+function params(params, o) {
+  return Object.assign({}, params, o);
+}
+
+function testBackup(params, databaseName, outputStream, callback) {
+  if (params.useApi) {
+    app.backup(dbUrl(process.env.COUCH_URL, databaseName), outputStream, params.opts, function(err, data) {
+      if (err) {
+        callback(err);
+      } else {
+        console.log(data);
+        callback();
+      }
+    });
+  } else {
+    // Set up default args
+    const args = ['../bin/couchbackup.bin.js', '--db', databaseName];
+    if (params.opts && params.opts.mode) {
+      args.push('--mode');
+      args.push(params.opts.mode);
+    }
+
+    // Note use spawn not fork for stdio options not supported with fork in Node 4.x
+    const backup = spawn('node', args, {'stdio': ['ignore', 'pipe', 'inherit']});
+    // Pipe the stdout to the supplied outputStream
+    backup.stdout.pipe(outputStream);
+    backup.on('exit', function(code) {
+      try {
+        assert.equal(code, 0, 'The backup should exit normally.');
+        callback();
+      } catch (err) {
+        callback(err);
+      }
+    });
+    backup.on('error', function(err) {
+      callback(err);
+    });
+  }
+}
+
+function testRestore(params, inputStream, databaseName, callback) {
+  if (params.useApi) {
+    app.restore(inputStream, dbUrl(process.env.COUCH_URL, databaseName), null, function(err, data) {
+      if (err) {
+        callback(err);
+      } else {
+        console.log(data);
+        callback();
+      }
+    });
+  } else {
+    // Note use spawn not fork for stdio options not supported with fork in Node 4.x
+    const restore = spawn('node', ['../bin/couchrestore.bin.js', '--db', databaseName], {'stdio': ['pipe', 'inherit', 'inherit']});
+    // Pipe to write the readable inputStream into stdin
+    inputStream.pipe(restore.stdin);
+    restore.on('exit', function(code) {
+      try {
+        assert.equal(code, 0, 'The restore should exit normally.');
+        callback();
+      } catch (err) {
+        callback(err);
+      }
+    });
+    restore.on('error', function(err) {
+      callback(err);
+    });
+  }
+}
+
+// Serial backup and restore via a file on disk
+function testBackupAndRestoreViaFile(params, srcDb, backupFile, targetDb, callback) {
+  const output = fs.createWriteStream(backupFile);
+  output.on('open', function() {
+    testBackup(params, srcDb, output, function(err) {
+      if (err) {
+        callback(err);
+      } else {
+        const input = fs.createReadStream(backupFile);
+        input.on('open', function() {
+          testRestore(params, input, targetDb, function(err) {
+            if (err) {
+              callback(err);
+            } else {
+              dbCompare(srcDb, targetDb, callback);
+            }
+          });
+        });
+      }
+    });
+  });
+}
+
+function testDirectBackupAndRestore(params, srcDb, targetDb, callback) {
+  // Allow a 64 MB highWaterMark for the passthrough during testing
+  const passthrough = new stream.PassThrough({highWaterMark: 67108864});
+  testBackupAndRestore(params, srcDb, passthrough, passthrough, targetDb, callback);
+}
+
+function testBackupAndRestore(params, srcDb, backupStream, restoreStream, targetDb, callback) {
+  testBackup(params, srcDb, backupStream, function(err) {
+    if (err) {
+      callback(err);
+    }
+  });
+  testRestore(params, restoreStream, targetDb, function(err) {
+    if (err) {
+      callback(err);
+    } else {
+      dbCompare(srcDb, targetDb, callback);
+    }
+  });
+}
+
+function dbCompare(db1Name, db2Name, callback) {
+  const comparison = spawn(`./${process.env.DBCOMPARE_NAME}-${process.env.DBCOMPARE_VERSION}/bin/${process.env.DBCOMPARE_NAME}`,
+    [process.env.COUCH_URL, db1Name, process.env.COUCH_URL, db2Name], {'stdio': 'inherit'});
+  comparison.on('exit', function(code) {
+    try {
+      assert.equal(code, 0, 'The database comparison should succeed.');
+      callback();
+    } catch (err) {
+      callback(err);
+    }
+  });
+  comparison.on('error', function(err) {
+    callback(err);
+  });
+}
+
+function sortByIdThenRev(o1, o2) {
+  if (o1._id < o2._id) return -1;
+  if (o1._id > o2._id) return 1;
+  if (o1._rev < o2._rev) return -1;
+  if (o1._rev > o2._rev) return 1;
+  return 0;
+}
+
+function readSortAndDeepEqual(actualContentPath, expectedContentPath, callback) {
+  const backupContent = JSON.parse(fs.readFileSync(actualContentPath, 'utf8'));
+  const expectedContent = JSON.parse(fs.readFileSync(expectedContentPath, 'utf8'));
+  // Array order of the docs is important for equality, but not for backup
+  backupContent.sort(sortByIdThenRev);
+  expectedContent.sort(sortByIdThenRev);
+  // Assert that the backup matches the expected
+  try {
+    assert.deepEqual(backupContent, expectedContent);
+    callback();
+  } catch (err) {
+    callback(err);
+  }
+}
+
+function timeoutFilter(context, timeout) {
+  // Default to a limit of 1 minute for tests
+  const limit = (typeof process.env.TEST_LIMIT !== 'undefined') ? parseInt(process.env.TEST_LIMIT) : 60;
+  timeout = (!timeout) ? 60 : timeout;
+  if (timeout <= limit) {
+    // Set the mocha timeout
+    context.timeout(timeout * 1000);
+  } else {
+    // Skip the test if it is expected to run for longer than the limit
+    context.skip();
+  }
+}
+
+module.exports = {
+  scenario: scenario,
+  p: params,
+  timeoutFilter: timeoutFilter,
+  dbCompare: dbCompare,
+  readSortAndDeepEqual: readSortAndDeepEqual,
+  testBackup: testBackup,
+  testRestore: testRestore,
+  testBackupAndRestore: testBackupAndRestore,
+  testDirectBackupAndRestore: testDirectBackupAndRestore,
+  testBackupAndRestoreViaFile: testBackupAndRestoreViaFile
+};
--- citest/test.js
+++ citest/test.js
@@ -1,56 +1,23 @@
-/* global describe it beforeEach afterEach */
+/* global describe it */
 'use strict';
 
-const assert = require('assert');
-const spawn = require('child_process').spawn;
 const fs = require('fs');
-const cloudant = require('cloudant')({url: process.env.COUCH_URL});
-const uuid = require('uuid/v4');
-const app = require('../app.js');
-const dbUrl = require('../includes/cliutils.js').databaseUrl;
-const stream = require('stream');
-
-var dbName;
-beforeEach('Create test database', function(done) {
-  // Allow 10 seconds to create the DB
-  this.timeout(10 * 1000);
-  dbName = 'couchbackup_test_' + uuid();
-  console.log(`Database name ${dbName}`);
-  cloudant.db.create(dbName, function(err) {
-    if (err) {
-      done(err);
-    } else {
-      done();
-    }
-  });
-});
-
-afterEach('Delete test database', function(done) {
-  // Allow 10 seconds to delete the DB
-  this.timeout(10 * 1000);
-  cloudant.db.destroy(dbName, function(err) {
-    if (err) {
-      done(err);
-    } else {
-      done();
-    }
-  });
-});
+const u = require('./citestutils.js');
 
 [{useApi: true}, {useApi: false}].forEach(function(params) {
-  describe(`Basic backup and restore ${(params.useApi) ? 'using API' : 'using CLI'}`, function() {
-    // Allow up to 40 s to backup and compare (it should be much faster)!
-    this.timeout(40 * 1000);
-    const actualBackup = `./${uuid()}_animaldb_actual.json`;
+  describe(u.scenario('Basic backup and restore', params), function() {
     it('should backup animaldb to a file correctly', function(done) {
+      // Allow up to 40 s to backup and compare (it should be much faster)!
+      u.timeoutFilter(this, 40);
+      const actualBackup = `./${this.fileName}`;
       // Create a file and backup to it
       const output = fs.createWriteStream(actualBackup);
       output.on('open', function() {
-        testBackup(params.useApi, 'animaldb', output, function(err) {
+        u.testBackup(params, 'animaldb', output, function(err) {
           if (err) {
             done(err);
           } else {
-            readSortAndDeepEqual(actualBackup, './animaldb_expected.json', done);
+            u.readSortAndDeepEqual(actualBackup, './animaldb_expected.json', done);
           }
         });
       });
@@ -58,14 +25,15 @@ afterEach('Delete test database', function(done) {
 
     it('should restore animaldb to a database correctly', function(done) {
       // Allow up to 60 s to restore and compare (again it should be faster)!
-      this.timeout(60 * 1000);
+      u.timeoutFilter(this, 60);
       const input = fs.createReadStream('animaldb_expected.json');
+      const dbName = this.dbName;
       input.on('open', function() {
-        testRestore(params.useApi, input, dbName, function(err) {
+        u.testRestore(params, input, dbName, function(err) {
           if (err) {
             done(err);
           } else {
-            dbCompare('animaldb', dbName, done);
+            u.dbCompare('animaldb', dbName, done);
           }
         });
       });
@@ -73,152 +41,33 @@ afterEach('Delete test database', function(done) {
 
     it('should execute a shallow mode backup successfully', function(done) {
       // Allow 30 s
-      this.timeout(30 * 1000);
-      const actualBackup = `./${uuid()}_animaldb_actual_shallow.json`;
+      u.timeoutFilter(this, 30);
+      const actualBackup = `./${this.fileName}`;
       const output = fs.createWriteStream(actualBackup);
+      // Add the shallow mode option
+      const p = u.p(params, {opts: {mode: 'shallow'}});
       output.on('open', function() {
-        testBackup(params.useApi, 'animaldb', output, function(err) {
+        u.testBackup(p, 'animaldb', output, function(err) {
           if (err) {
             done(err);
           } else {
-            readSortAndDeepEqual(actualBackup, './animaldb_expected_shallow.json', done);
+            u.readSortAndDeepEqual(actualBackup, './animaldb_expected_shallow.json', done);
           }
-        }, {mode: 'shallow'});
+        });
       });
     });
   });
 
-  describe(`End to end backup and restore ${(params.useApi) ? 'using API' : 'using CLI'}`, function() {
+  describe(u.scenario('End to end backup and restore', params), function() {
     it('should backup and restore animaldb', function(done) {
       // Allow up to 60 s for backup and restore of animaldb
-      this.timeout(60 * 1000);
-      testBackupAndRestore(params.useApi, 'animaldb', dbName, done);
+      u.timeoutFilter(this, 60);
+      u.testDirectBackupAndRestore(params, 'animaldb', this.dbName, done);
     });
     it('should backup and restore largedb1g', function(done) {
       // Allow up to 10 m for backup and restore of largedb1g
-      this.timeout(10 * 60 * 1000);
-      testBackupAndRestore(params.useApi, 'largedb1g', dbName, done);
+      u.timeoutFilter(this, 10 * 60);
+      u.testDirectBackupAndRestore(params, 'largedb1g', this.dbName, done);
     });
   });
 });
-
-function testBackup(useApi, databaseName, outputStream, callback, opts) {
-  if (useApi) {
-    app.backup(dbUrl(process.env.COUCH_URL, databaseName), outputStream, opts, function(err, data) {
-      if (err) {
-        callback(err);
-      } else {
-        console.log(data);
-        callback();
-      }
-    });
-  } else {
-    // Set up default args
-    const args = ['../bin/couchbackup.bin.js', '--db', databaseName];
-    if (opts && opts.mode) {
-      args.push('--mode');
-      args.push(opts.mode);
-    }
-
-    // Note use spawn not fork for stdio options not supported with fork in Node 4.x
-    const backup = spawn('node', args, {'stdio': ['ignore', 'pipe', 'inherit']});
-    // Pipe the stdout to the supplied outputStream
-    backup.stdout.pipe(outputStream);
-    backup.on('exit', function(code) {
-      try {
-        assert.equal(code, 0, 'The backup should exit normally.');
-        callback();
-      } catch (err) {
-        callback(err);
-      }
-    });
-    backup.on('error', function(err) {
-      callback(err);
-    });
-  }
-}
-
-function testRestore(useApi, inputStream, databaseName, callback) {
-  if (useApi) {
-    app.restore(inputStream, dbUrl(process.env.COUCH_URL, databaseName), null, function(err, data) {
-      if (err) {
-        callback(err);
-      } else {
-        console.log(data);
-        callback();
-      }
-    });
-  } else {
-    // Note use spawn not fork for stdio options not supported with fork in Node 4.x
-    const restore = spawn('node', ['../bin/couchrestore.bin.js', '--db', databaseName], {'stdio': ['pipe', 'inherit', 'inherit']});
-    // Pipe to write the readable inputStream into stdin
-    inputStream.pipe(restore.stdin);
-    restore.on('exit', function(code) {
-      try {
-        assert.equal(code, 0, 'The restore should exit normally.');
-        callback();
-      } catch (err) {
-        callback(err);
-      }
-    });
-    restore.on('error', function(err) {
-      callback(err);
-    });
-  }
-}
-
-function testBackupAndRestore(useApi, srcDb, targetDb, callback) {
-  // Allow a 64 MB highWaterMark for the passthrough during testing
-  const passthrough = new stream.PassThrough({highWaterMark: 67108864});
-  testBackup(useApi, srcDb, passthrough, function(err) {
-    if (err) {
-      callback(err);
-    }
-  });
-  testRestore(useApi, passthrough, targetDb, function(err) {
-    if (err) {
-      callback(err);
-    } else {
-      dbCompare(srcDb, dbName, callback);
-    }
-  });
-}
-
-function dbCompare(db1Name, db2Name, callback) {
-  const comparison = spawn(`./${process.env.DBCOMPARE_NAME}-${process.env.DBCOMPARE_VERSION}/bin/${process.env.DBCOMPARE_NAME}`,
-    [process.env.COUCH_URL, db1Name, process.env.COUCH_URL, db2Name], {'stdio': 'inherit'});
-  comparison.on('exit', function(code) {
-    try {
-      assert.equal(code, 0, 'The database comparison should succeed.');
-      callback();
-    } catch (err) {
-      callback(err);
-    }
-  });
-  comparison.on('error', function(err) {
-    callback(err);
-  });
-}
-
-function sortByIdThenRev(o1, o2) {
-  if (o1._id < o2._id) return -1;
-  if (o1._id > o2._id) return 1;
-  if (o1._rev < o2._rev) return -1;
-  if (o1._rev > o2._rev) return 1;
-  return 0;
-}
-
-function readSortAndDeepEqual(actualContentPath, expectedContentPath, callback) {
-  const backupContent = require(actualContentPath);
-  const expectedContent = require(expectedContentPath);
-  // Array order of the docs is important for equality, but not for backup
-  backupContent.sort(sortByIdThenRev);
-  expectedContent.sort(sortByIdThenRev);
-  // Assert that the backup matches the expected
-  try {
-    assert.deepEqual(backupContent, expectedContent);
-    callback();
-  } catch (err) {
-    callback(err);
-  }
-}
