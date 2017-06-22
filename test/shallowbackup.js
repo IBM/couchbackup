b064a07606eda7fedeb9c2bc2a32302ed2985956
Sam Smith <samsmith@linux.vnet.ibm.com>
2017-06-22 16:48:35 +0100

    Handle fatal errors in shallow backup

---
+++ test/shallowbackup.js
@@ -0,0 +1,144 @@
+// Copyright © 2017 IBM Corp. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* global describe it beforeEach */
+'use strict';
+
+const assert = require('assert');
+const backup = require('../includes/shallowbackup.js');
+const fs = require('fs');
+const nock = require('nock');
+
+describe('#unit Perform backup using shallow backup', function() {
+  const dbUrl = 'http://localhost:5984/animaldb';
+
+  beforeEach('Reset nocks', function() {
+    nock.cleanAll();
+  });
+
+  it('should perform a shallow backup', function(done) {
+    var couch = nock(dbUrl)
+      // batch 1
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_1.json', 'utf8')))
+      // batch 2
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"badger\\u0000"'})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_2.json', 'utf8')))
+      // batch 3
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"kookaburra\\u0000"'})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_3.json', 'utf8')))
+      // batch 4
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"snipe\\u0000"'})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_4.json', 'utf8')));
+
+    backup(dbUrl, 3, 1, null, null)
+      .on('error', function(err) {
+        assert.fail(err);
+      })
+      .on('received', function(data) {
+        if (data.batch === 3) {
+          assert.equal(data.length, 2); // smaller last batch
+        } else {
+          assert.equal(data.length, 3);
+        }
+      })
+      .on('finished', function(data) {
+        assert.equal(data.total, 11);
+        couch.isDone();
+        done();
+      });
+  });
+
+  it('should perform a shallow backup with transient error', function(done) {
+    var couch = nock(dbUrl)
+      // batch 1
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_1.json', 'utf8')))
+      // batch 2
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"badger\\u0000"'})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_2.json', 'utf8')))
+      // batch 3 - transient error
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"kookaburra\\u0000"'})
+      .reply(500, {error: 'Internal Server Error'})
+      // batch 3 - retry
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"kookaburra\\u0000"'})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_3.json', 'utf8')))
+      // batch 4
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"snipe\\u0000"'})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_4.json', 'utf8')));
+
+    var errCount = 0;
+
+    backup(dbUrl, 3, 1, null, null)
+      .on('error', function(err) {
+        errCount++;
+        assert.equal(err.name, 'HTTPError');
+      })
+      .on('received', function(data) {
+        if (data.batch === 3) {
+          assert.equal(data.length, 2); // smaller last batch
+        } else {
+          assert.equal(data.length, 3);
+        }
+      })
+      .on('finished', function(data) {
+        assert.equal(data.total, 11);
+        couch.isDone();
+        assert.equal(errCount, 1);
+        done();
+      });
+  });
+
+  it('should fail to perform a shallow backup on fatal error', function(done) {
+    var couch = nock(dbUrl)
+      // batch 1
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_1.json', 'utf8')))
+      // batch 2
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"badger\\u0000"'})
+      .reply(200, JSON.parse(fs.readFileSync('./test/fixtures/animaldb_all_docs_2.json', 'utf8')))
+      // batch 3 - fatal error
+      .get('/_all_docs')
+      .query({limit: 3, include_docs: true, startkey: '"kookaburra\\u0000"'})
+      .reply(401, {error: 'Unauthorized'});
+
+    var errCount = 0;
+
+    backup(dbUrl, 3, 1, null, null)
+      .on('error', function(err) {
+        errCount++;
+        assert.equal(err.name, 'Unauthorized');
+      })
+      .on('received', function(data) {
+        assert.equal(data.length, 3);
+      })
+      .on('finished', function(data) {
+        assert.equal(data.total, 6);
+        couch.isDone();
+        assert.equal(errCount, 1);
+        done();
+      });
+  });
+});
