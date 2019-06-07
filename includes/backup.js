bb08be238af907ed2f8c770cc745cf2bc619623d
Sam Smith <samsmith@linux.vnet.ibm.com>
2019-06-07 12:00:33 +0100

    [Upgrade async@3.0.1] Register callback with `q.drain` method.

--- includes/backup.js
+++ includes/backup.js
@@ -1,4 +1,4 @@
-// Copyright © 2017, 2018 IBM Corp. All rights reserved.
+// Copyright © 2017, 2019 IBM Corp. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -254,9 +254,9 @@ function processBatchSet(db, parallelism, log, batches, ee, start, grandtotal, c
     q.push(batches[i]);
   }
 
-  q.drain = function() {
+  q.drain(function() {
     callback(null, { total: total });
-  };
+  });
 }
 
 /**
