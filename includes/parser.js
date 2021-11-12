efdca98d3c5969d11f918ed0774521fbf65664bc
Veronika Matyus <veronika.matyus@ibm.com>
2021-11-12 16:28:14 +0100

    Update to be compatible with commander 7.0.0
    Command options stored not as properties anymore, their handling changed based on migration guide: https://github.com/tj/commander.js/blob/master/CHANGELOG.md#migration-tips-1

--- includes/parser.js
+++ includes/parser.js
@@ -62,7 +62,7 @@ function parseBackupArgs() {
     .parse(process.argv);
 
   // Remove defaults that don't apply when using shallow mode
-  if (program.mode === 'shallow' || envVarOptions.mode === 'shallow') {
+  if (program.opts().mode === 'shallow' || envVarOptions.mode === 'shallow') {
     delete defaults.parallelism;
     delete defaults.log;
     delete defaults.resume;
@@ -70,7 +70,7 @@ function parseBackupArgs() {
 
   // Apply the options in order so that the CLI overrides env vars and env variables
   // override defaults.
-  const opts = Object.assign({}, defaults, envVarOptions, program);
+  const opts = Object.assign({}, defaults, envVarOptions, program.opts());
 
   if (opts.resume && (opts.log === defaults.log)) {
     // If resuming and the log file arg is the newly generated tmp name from defaults then we know that --log wasn't specified.
@@ -114,7 +114,7 @@ function parseRestoreArgs() {
 
   // Apply the options in order so that the CLI overrides env vars and env variables
   // override defaults.
-  const opts = Object.assign({}, defaults, envVarOptions, program);
+  const opts = Object.assign({}, defaults, envVarOptions, program.opts());
 
   return opts;
 }
