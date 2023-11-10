8d5367de71531e92a5a556cec0064426db993505
Rich Ellis <ricellis@users.noreply.github.com>
2023-11-10 15:54:40 +0000

    test: update spoolchanges tests
    Update to use new follower based spoolchanges.
    Use httpServer instead of nock for long running tests with
    lots of changes requests because nock appears to leak memory
    in those situations.
    
    Co-authored-by: Esteban Laver <10552966+emlaver@users.noreply.github.com>

--- test/spoolchanges.js
+++ test/spoolchanges.js
@@ -12,179 +12,189 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* global describe it */
+/* global afterEach beforeEach describe it */
 'use strict';
 
 const assert = require('assert');
 const nock = require('nock');
+const http = require('node:http');
 const request = require('../includes/request.js');
 const changes = require('../includes/spoolchanges.js');
 
-const url = 'http://localhost:7777';
+const host = 'localhost';
+// To avoid clashes between multiple runs use a given port (converted to a number) if configured
+const port = +process.env.COUCHBACKUP_MOCK_SERVER_PORT || 7777;
+const url = `http://${host}:${port}`;
 const dbName = 'fakenockdb';
 const longTestTimeout = 3000;
 
 const db = request.client(`${url}/${dbName}`, { parallelism: 1 });
 
 const seqSuffix = Buffer.alloc(124, 'abc123').toString('base64');
-function provideChanges(batchSize, totalChanges, fullResponse = false) {
-  let pending = totalChanges;
-  const sparseResultsArray = Array(batchSize).fill({
-    seq: null,
-    id: 'doc',
-    changes: [{ rev: '1-abcdef0123456789abcdef0123456789' }]
+
+describe('Check spool changes', function() {
+  describe('#unit error cases', function() {
+    it('should terminate on request error', async function() {
+      nock(url)
+        .post(`/${dbName}/_changes`)
+        .query(true)
+        .times(3)
+        .replyWithError({ code: 'ECONNRESET', message: 'socket hang up' });
+
+      // Note this is setting changes follower tolerance to 0
+      // so that the error is not suppressed beyond 3 configured retries
+      // in the underlying SDK call, follower will not retry
+      return changes(db, '/dev/null', 500, 0).catch((err) => {
+        assert.strictEqual(err.name, 'SpoolChangesError');
+        assert.strictEqual(err.message, `Failed changes request - socket hang up: post ${url}/${dbName}/_changes`);
+        assert.ok(nock.isDone());
+      });
+    }).timeout(longTestTimeout);
+
+    it('should terminate on bad HTTP status code response', async function() {
+      nock(url)
+        .post(`/${dbName}/_changes`)
+        .query(true)
+        .times(3)
+        .reply(500, function(uri, requestBody) {
+          this.req.response.statusMessage = 'Internal Server Error';
+          return { error: 'foo', reason: 'bar' };
+        });
+
+      // Note this is setting changes follower tolerance to 0
+      // so that the error is not suppressed beyond 3 configured retries
+      // in the underlying SDK call, follower will not retry
+      return changes(db, '/dev/null', 500, 0).catch((err) => {
+        assert.strictEqual(err.name, 'HTTPFatalError');
+        assert.strictEqual(err.message, `500 Internal Server Error: post ${url}/${dbName}/_changes - Error: foo, Reason: bar`);
+        assert.ok(nock.isDone());
+      });
+    }).timeout(longTestTimeout);
   });
-  nock(url)
-    .post(`/${dbName}/_changes`)
-    .query(true)
-    .times(totalChanges / batchSize + (totalChanges % batchSize > 0 ? 1 : 0))
-    .reply(200, (uri, requestBody) => {
+
+  describe('success cases', function() {
+    let server;
+    let batchSize;
+    let totalChanges;
+    let fullResponse = false;
+    let sparseResultsArray;
+    let remainingMockCalls;
+    let pending;
+
+    beforeEach('Start server', function(done) {
+      server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(getReply());
+      });
+      server.listen(port, host, done);
+    });
+
+    afterEach('Stop server', function(done) {
+      server.closeAllConnections();
+      server.close(done);
+    });
+
+    function setupTestSize() {
+      remainingMockCalls = totalChanges / batchSize + (totalChanges % batchSize > 0 ? 1 : 0);
+      pending = totalChanges;
+      sparseResultsArray = (!fullResponse)
+        ? Array(batchSize).fill({
+          seq: null,
+          id: 'doc',
+          changes: [{ rev: '1-abcdef0123456789abcdef0123456789' }]
+        })
+        : [];
+    }
+
+    function makeChangeItem(seq, index) {
+      return {
+        seq: `${seq + index}-${seqSuffix}`,
+        id: `doc${seq + index}`,
+        changes: [{ rev: '1-abcdef0123456789abcdef0123456789' }]
+      };
+    }
+
+    function getResults(batchSize, seq) {
+      return Array.from(Array(batchSize).fill(seq), makeChangeItem);
+    }
+
+    function getReply() {
+      remainingMockCalls--;
       pending -= batchSize;
       const lastSeq = (totalChanges - pending);
       const seq = lastSeq - batchSize;
-      return {
-        results: fullResponse
-          ? Array.from(Array(batchSize), (_, i) => {
-            return {
-              seq: `${seq + i}-${seqSuffix}`,
-              id: `doc${seq + i}`,
-              changes: [{ rev: '1-abcdef0123456789abcdef0123456789' }]
-            };
-          })
-          : sparseResultsArray,
-        pending: pending,
+      return JSON.stringify({
+        results: fullResponse ? getResults(batchSize, seq) : sparseResultsArray,
+        pending,
         last_seq: `${lastSeq}-abc`
-      };
-    });
-}
+      });
+    }
 
-describe('#unit Check spool changes', function() {
-  it('should terminate on request error', async function() {
-    nock(url)
-      .post(`/${dbName}/_changes`)
-      .query(true)
-      .times(3)
-      .replyWithError({ code: 'ECONNRESET', message: 'socket hang up' });
+    describe('#unit shorter spool changes checks', function() {
+      it('should keep collecting changes', async function() {
+      // This test validates that spooling will correctly
+      // continue across multiple requests
+      // (4 batches of 100000 to be precise).
+      // This test might take up to 10 seconds
+        this.timeout(10 * 1000);
 
-    return new Promise((resolve, reject) => {
-      changes(db, '/dev/null', 500, null, function(err) {
-        try {
-          assert.strictEqual(err.name, 'SpoolChangesError');
-          assert.strictEqual(err.message, `Failed changes request - socket hang up: post ${url}/${dbName}/_changes`);
-          assert.ok(nock.isDone());
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
+        // Use full changes for this test
+        batchSize = 100000;
+        totalChanges = 400000;
+        fullResponse = true;
+        setupTestSize();
+        return changes(db, '/dev/null', 500).then(() => {
+          assert.equal(remainingMockCalls, 0, 'There should be the correct number of mock calls.');
+        });
+      });
+
+      it('should keep collecting sparse changes', async function() {
+      // This test checks that making thousands of requests doesn't
+      // make anything bad happen.
+      // This test might take up to 25 seconds
+        this.timeout(25 * 1000);
+        // Use sparse changes for this test and a batch size of 1
+        batchSize = 1;
+        totalChanges = 2500;
+        fullResponse = false;
+        setupTestSize();
+        return changes(db, '/dev/null', 500).then(() => {
+          assert.equal(remainingMockCalls, 0, 'There should be the correct number of mock calls.');
+        });
       });
     });
-  }).timeout(longTestTimeout);
 
-  it('should terminate on bad HTTP status code response', async function() {
-    nock(url)
-      .post(`/${dbName}/_changes`)
-      .query(true)
-      .times(3)
-      .reply(500, function(uri, requestBody) {
-        this.req.response.statusMessage = 'Internal Server Error';
-        return { error: 'foo', reason: 'bar' };
+    describe('Longer spool changes checks', function() {
+      it('#slow should keep collecting changes (25M)', async function() {
+      // This test might take up to 5 minutes
+        this.timeout(5 * 60 * 1000);
+        // Note changes spooling uses a constant batch size, we are setting
+        // a test value here and setting the bufferSize to match when changes
+        // is called
+        batchSize = 100000;
+        totalChanges = 25000000;
+        fullResponse = false;
+        setupTestSize();
+        // Use sparse changes for this test
+        return changes(db, '/dev/null', batchSize).then(() => {
+          assert.equal(remainingMockCalls, 0, 'There should be the correct number of mock calls.');
+        });
       });
-    return new Promise((resolve, reject) => {
-      changes(db, '/dev/null', 500, null, function(err) {
-        try {
-          assert.strictEqual(err.name, 'HTTPFatalError');
-          assert.strictEqual(err.message, `500 Internal Server Error: post ${url}/${dbName}/_changes - Error: foo, Reason: bar`);
-          assert.ok(nock.isDone());
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
-  }).timeout(longTestTimeout);
 
-  it('should keep collecting changes', async function() {
-    // This test validates that spooling will correctly
-    // continue across multiple requests
-    // (4 batches of 100000 to be precise).
-    // This test might take up to 10 seconds
-    this.timeout(10 * 1000);
-
-    // Use full changes for this test
-    provideChanges(100000, 400000, true);
-    return new Promise((resolve, reject) => {
-      changes(db, '/dev/null', 500, null, function(err) {
-        try {
-          assert.ok(!err);
-          assert.ok(nock.isDone());
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
-  });
-
-  it('should keep collecting sparse changes', async function() {
-    // This test checks that making thousands of requests doesn't
-    // make anything bad happen.
-    // This test might take up to 25 seconds
-    this.timeout(25 * 1000);
-    // Use sparse changes for this test and a batch size of 1
-    provideChanges(1, 2500);
-    return new Promise((resolve, reject) => {
-      changes(db, '/dev/null', 500, null, function(err) {
-        try {
-          assert.ok(!err);
-          assert.ok(nock.isDone());
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
-  });
-});
-
-describe('Longer spool changes checks', function() {
-  it('#slow should keep collecting changes (25M)', async function() {
-    // This test might take up to 5 minutes
-    this.timeout(5 * 60 * 1000);
-    // Note changes spooling uses a constant batch size, we are setting
-    // a test value here and setting the buffer to match
-    const batch = 100000;
-    // Use sparse changes for this test
-    provideChanges(batch, 25000000);
-    return new Promise((resolve, reject) => {
-      changes(db, '/dev/null', batch, null, function(err) {
-        try {
-          assert.ok(!err);
-          assert.ok(nock.isDone());
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
-  });
-
-  it('#slower should keep collecting changes (500M)', async function() {
-    // This test might take up to 90 minutes
-    this.timeout(90 * 60 * 1000);
-    // Note changes spooling uses a constant batch size, we are setting
-    // a test value here and setting the buffer to match
-    const batch = 1000000;
-    // Use full changes for this test to exercise load
-    provideChanges(batch, 500000000, true);
-    return new Promise((resolve, reject) => {
-      changes(db, '/dev/null', batch, null, function(err) {
-        try {
-          assert.ok(!err);
-          assert.ok(nock.isDone());
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
+      it('#slower should keep collecting changes (500M)', async function() {
+      // This test might take up to 90 minutes
+        this.timeout(90 * 60 * 1000);
+        // Note changes spooling uses a constant batch size, we are setting
+        // a test value here and setting the bufferSize to match when changes
+        // is called
+        batchSize = 1000000;
+        totalChanges = 500000000;
+        // Use full changes for this test to exercise load
+        fullResponse = true;
+        setupTestSize();
+        return changes(db, '/dev/null', batchSize).then(() => {
+          assert.equal(remainingMockCalls, 0, 'There should be the correct number of mock calls.');
+        });
       });
     });
   });
