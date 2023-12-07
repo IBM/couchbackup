5b4ad515e19baf7c91c0ea26423f26becc23efb4
Rich Ellis <ricellis@users.noreply.github.com>
2023-12-07 09:53:15 +0000

    refactor: resume log file handling

--- includes/logfilesummary.js
+++ includes/logfilesummary.js
@@ -1,4 +1,4 @@
-// Copyright © 2017 IBM Corp. All rights reserved.
+// Copyright © 2017, 2023 IBM Corp. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,80 +13,50 @@
 // limitations under the License.
 'use strict';
 
-const fs = require('fs');
-const stream = require('stream');
+const { createReadStream } = require('node:fs');
+const { Writable } = require('node:stream');
+const { pipeline } = require('node:stream/promises');
 const { Liner } = require('./liner.js');
-
-const onLine = function(onCommand, getDocs) {
-  const change = new stream.Transform({ objectMode: true });
-
-  change._transform = function(line, encoding, done) {
-    if (line && line[0] === ':') {
-      const obj = {
-        command: null,
-        batch: null,
-        docs: []
-      };
-
-      let matches;
-
-      // extract command
-      matches = line.match(/^:([a-z_]+) ?/);
-      if (matches) {
-        obj.command = matches[1];
-      }
-
-      // extract batch
-      matches = line.match(/ batch([0-9]+)/);
-      if (matches) {
-        obj.batch = parseInt(matches[1]);
-      }
-
-      // extract doc ids
-      if (getDocs && obj.command === 't') {
-        const json = line.replace(/^.* batch[0-9]+ /, '').trim();
-        obj.docs = JSON.parse(json);
-      }
-      onCommand(obj);
-    }
-    done();
-  };
-  return change;
-};
+const { LogMapper } = require('./backupMappings.js');
+const { MappingStream } = require('./transforms.js');
 
 /**
  * Generate a list of remaining batches from a download file.
+ * Creates a summary containing a changesComplete boolean for
+ * if the :changes_complete log file entry was found and a map
+ * of pending batch numbers that have yet to be backed up
+ * (i.e. the difference of :t and :d log file entries).
  *
  * @param {string} log - log file name
- * @param {function} callback - callback with err, {changesComplete: N, batches: N}.
- *  changesComplete signifies whether the log file appeared to
- *  have completed reading the changes feed (contains :changes_complete).
- *  batches are remaining batch IDs for download.
+ * @returns a log summary object
  */
-module.exports = function(log, callback) {
-  // our sense of state
-  const state = {
+module.exports = async function(log) {
+  const logMapper = new LogMapper();
+  const state = { changesComplete: false, batches: new Map() };
 
-  };
-  let changesComplete = false;
-
-  // called with each line from the log file
-  const onCommand = function(obj) {
-    if (obj.command === 't') {
-      state[obj.batch] = true;
-    } else if (obj.command === 'd') {
-      delete state[obj.batch];
-    } else if (obj.command === 'changes_complete') {
-      changesComplete = true;
-    }
-  };
-
-  // stream through the previous log file
-  fs.createReadStream(log)
-    .pipe(new Liner())
-    .pipe(onLine(onCommand, false))
-    .on('finish', function() {
-      const obj = { changesComplete: changesComplete, batches: state };
-      callback(null, obj);
-    });
+  await pipeline(
+    createReadStream(log), // read the log file
+    new Liner(true), // split it into lines
+    new MappingStream(logMapper.logLineToMetadata), // parse line to metadata
+    new Writable({
+      objectMode: true,
+      write: (metadata, encoding, callback) => {
+        switch (metadata.command) {
+          case 't':
+            state.batches.set(metadata.batch, true);
+            break;
+          case 'd':
+            state.batches.delete(metadata.batch);
+            break;
+          case 'changes_complete':
+            state.changesComplete = true;
+            break;
+          default:
+            break;
+        }
+        callback();
+      }
+    }) // Save the done batch number in an array
+  );
+  return state;
 };
